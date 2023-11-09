058f671ac60659af09002fce4ea67b3f78f7e946
Muhammad Awais <awais705@gmail.com>
2023-11-09 19:01:56 +0500

    Create db.py

---
+++ flast_part_1_assignment/db.py
@@ -0,0 +1,29 @@
+import pymysql
+
+
+def mysqlconnect():
+    # To connect MySQL database
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password="admin",  # empty password
+        db='exercise_class_1',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("db connected")
+    return conn
+    # cur = conn.cursor()
+    # cur.execute("select @@version")
+    # output = cur.fetchall()
+    # print(output)
+
+    # To close the connection
+
+
+def disconnect(conn):
+    conn.close()
+
+
+# Driver Code
+if __name__ == "__main__":
+    mysqlconnect()
